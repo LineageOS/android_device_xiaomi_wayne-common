e25cbf684c1208295cba49d94f0d6937f3ff868a
Chirayu Desai <chirayudesai1@gmail.com>
2020-06-03 02:55:10 +0530

    wayne-common: Set screen density to 440
    * This matches another 1080x2160 device, flame.
    * Also matches how things look on stock
    * Makes it look much nicer in general
    * Can still go back to previous by Settings -> Display -> Display Size -> Larger (+)
    
    Change-Id: Ib12545032daaf59a1b54e293069b0f3678332a70

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -27,6 +27,9 @@ include device/xiaomi/sdm660-common/BoardConfigCommon.mk
 
 WAYNE_PATH := device/xiaomi/wayne-common
 
+# Display
+TARGET_SCREEN_DENSITY := 440
+
 # HIDL
 DEVICE_MANIFEST_FILE += $(WAYNE_PATH)/manifest.xml
 
